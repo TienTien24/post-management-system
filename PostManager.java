a1c32422877330971891b3783dd70476ac75b1e8
TienTien <thuytiena2404@gmail.com>
2026-03-13 17:31:48 +0700

    Add PostManager class (Dev version)

---
+++ PostManager.java
@@ -0,0 +1,12 @@
+public class PostManager {
+
+    public int getTotalPosts(int a, int b){
+        return a - b; // BUG: phải là a + b
+    }
+
+    public static void main(String[] args) {
+        PostManager pm = new PostManager();
+        int total = pm.getTotalPosts(5,3);
+        System.out.println("Total posts: " + total);
+    }
+}
