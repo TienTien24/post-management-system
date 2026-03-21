e9e18dd1c3b88af7aeef1706ca84ea6d151aabc1
trangtrang0031@gmail.com <trangtrang0031@gmail.com>
2026-03-21 18:35:08 +0700

    Fix bug JDBC connection

--- DBConnection.java
+++ DBConnection.java
@@ -2,10 +2,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
+
     public static Connection getConnection() throws Exception {
         return DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/post_management",
                 "root",
                 "");
+    }
+
+    public static void main(String[] args) {
+        try {
+            Connection conn = getConnection();
+            if (conn != null) {
+                System.out.println("Kết nối thành công!");
+            }
+        } catch (Exception e) {
+            System.out.println("Kết nối thất bại!");
+            e.printStackTrace();
+        }
     }
 }
